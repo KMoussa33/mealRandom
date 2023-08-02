688f729e94684c083270db1a935ad3b71c28df8a
KMoussa33 <kelsey.moussa@gmail.com>
2023-08-02 11:36:49 -0400

    creating meal sets

---
+++ meals.py
@@ -0,0 +1,4 @@
+breakfast = {'french toast', 'arepas', 'avocado toast', 'toast', 'omelette', 'egg sandwiches', 'egg in the hole', 'hashbrown patties', 'hashbrowns and eggs'}
+lunches = {'buffalo chicken sandwiches with fries', 'grilled cheese', 'tater tots'}
+dinners = {'chicken shawarma', 'chicken tikka', 'spicy chicken pasta', 'coconut curry shrimp', 'shrimp and rice', 'lamb kofta', 'greek sheet pan chicken', 'cold pasta salad', 'mediterranean salad', 'spaghetti and meatballs', 'baked feta salmon', 'keto burgers', 'garlic parmesan chicken and potatoes', 'baked cajun chicken', 'pesto pasta', 'chicken salad'}
+dessert = {'chocolate chip cookies', 'peanut butter blossoms', 'pumpkin oat muffins', 'mug cookies', 'ice cream', 'popsicles'}
