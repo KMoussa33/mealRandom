7bb121bca0e2e440ff2574c62e29f2b16fcdeeb0
KMoussa33 <kelsey.moussa@gmail.com>
2023-08-03 14:01:22 -0400

    snacks

--- meals.py
+++ meals.py
@@ -2,3 +2,4 @@ breakfast = {'french toast', 'arepas', 'avocado toast', 'toast', 'omelette', 'eg
 lunches = {'buffalo chicken sandwiches with fries', 'grilled cheese', 'tater tots'}
 dinners = {'chicken shawarma', 'chicken tikka', 'spicy chicken pasta', 'coconut curry shrimp', 'shrimp and rice', 'lamb kofta', 'greek sheet pan chicken', 'cold pasta salad', 'mediterranean salad', 'spaghetti and meatballs', 'baked feta salmon', 'keto burgers', 'garlic parmesan chicken and potatoes', 'baked cajun chicken', 'pesto pasta', 'chicken salad'}
 dessert = {'chocolate chip cookies', 'peanut butter blossoms', 'pumpkin oat muffins', 'mug cookies', 'ice cream', 'popsicles'}
+snacks = {'salt & pepper chips', 'hummus and pita', 'yogurt', 'cheese and crackers', 'fruit', 'pickles and cream cheese', 'pringles'}
