d072f514e0927533de68c853e513080c02b54313
KMoussa33 <kelsey.moussa@gmail.com>
2023-08-03 14:03:55 -0400

    sides

--- meals.py
+++ meals.py
@@ -1,5 +1,11 @@
 breakfast = {'french toast', 'arepas', 'avocado toast', 'toast', 'omelette', 'egg sandwiches', 'egg in the hole', 'hashbrown patties', 'hashbrowns and eggs'}
+
 lunches = {'buffalo chicken sandwiches with fries', 'grilled cheese', 'tater tots'}
-dinners = {'chicken shawarma', 'chicken tikka', 'spicy chicken pasta', 'coconut curry shrimp', 'shrimp and rice', 'lamb kofta', 'greek sheet pan chicken', 'cold pasta salad', 'mediterranean salad', 'spaghetti and meatballs', 'baked feta salmon', 'keto burgers', 'garlic parmesan chicken and potatoes', 'baked cajun chicken', 'pesto pasta', 'chicken salad'}
+
+dinners = {'chicken shawarma', 'chicken tikka', 'spicy chicken pasta', 'coconut curry shrimp', 'shrimp and rice', 'lamb kofta', 'greek sheet pan chicken', 'cold pasta salad', 'mediterranean salad', 'spaghetti and meatballs', 'baked feta salmon', 'keto burgers', 'garlic parmesan chicken and potatoes', 'baked cajun chicken', 'pesto pasta', 'chicken salad', 'korean beef bowls'}
+
+sides = {'salad', 'roasted root vegetables', 'roasted potatoes & carrots', 'broccoli & gnocchi', 'corn', 'green beans', 'mixed vegetables', 'fries'}
+
 dessert = {'chocolate chip cookies', 'peanut butter blossoms', 'pumpkin oat muffins', 'mug cookies', 'ice cream', 'popsicles'}
+
 snacks = {'salt & pepper chips', 'hummus and pita', 'yogurt', 'cheese and crackers', 'fruit', 'pickles and cream cheese', 'pringles'}
